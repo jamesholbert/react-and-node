3b8da4298e6d696f0c630c6a8b6987bea0caaa91
James Holbert <arthesius@gmail.com>
2018-02-21 12:37:52 -0800

    got build working

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import path from 'path'
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
 const router = express.Router()
+
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+app.use(staticFiles)
+
 router.get('/cities', (req, res) => {
   const cities = [
     {name: 'New York City', population: 8175133},
@@ -12,8 +19,13 @@ router.get('/cities', (req, res) => {
   ]
   res.json(cities)
 })
+
 app.use(router)
-app.set('port', (process.env.PORT || 5000))
+
+// any routes not picked up by the server api will be handled by the react router
+app.use('/*', staticFiles)
+
+app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
